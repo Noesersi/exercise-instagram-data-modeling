f7acef3de36e3186ce72aa30757690636eff4fa5
Noé Ibáñez Torres <132618491+Noesersi@users.noreply.github.com>
2023-09-11 01:10:09 +0000

    relaciones

--- src/models.py
+++ src/models.py
@@ -16,20 +16,27 @@ class Users(Base):
     firstname = Column(String, nullable=False)
     lastname = Column(String, nullable=False)
     email = Column(String, nullable=False)
-
+    posts = relationship('Post', backref='user')
+    comments = relationship('Comment', backref='author')
+    # aqui me he liado un poco con los nombres, no estoy seguro de quien sigue a quien
+    following = relationship('Follower', foreign_keys=('Follower.user_from_id'), backref='follower')
+    followers = relationship('Follower', foreign_keys=('Follower.user_to_id'), backref='user')
 
 class Follower(Base):
     __tablename__ = 'Follower'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
     id= Column(Integer, primary_key=True)
     user_from_id = Column(Integer, ForeignKey('Users.id'))
     user_to_id = Column(Integer, nullable=False)
+    followed = relationship('Users', foreign_keys=('user_from_id'), backref='followers')
+    follower = relationship('Users', foreign_keys=('user_to_id'), backref='following')
 
 class Post(Base):
     __tablename__ = 'Post'
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey('Users.id'))
+    user = relationship('Users', backref='posts')
+    media = relationship('Media', backref='post')
+    comments = relationship('Comment', backref='post')
 
 
 class Media(Base):
@@ -38,6 +45,7 @@ class Media(Base):
     type = Column(Enum)
     url = Column(String)
     post_id = Column(Integer, ForeignKey('Post.id'))
+    post = relationship('Post', backref='media')
 
 class Comment(Base):
     __tablename__ = 'Comment'
@@ -45,6 +53,8 @@ class Comment(Base):
     Comment_text = Column(String)
     author_id = Column(Integer, ForeignKey('Users.id'))
     post_id = Column(Integer, ForeignKey('Post.id'))
+    author = relationship('Users', backref='comments')
+    post = relationship('Post', backref='comments')
 
 
     def to_dict(self):
